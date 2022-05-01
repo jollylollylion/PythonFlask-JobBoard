4159bd8da28c3073131512d345e170a5edbcd524
Joel Udeh <Joel.Udeh@impsoftware.co.uk>
2022-05-01 02:28:02 +0100

    Commit

--- jobs/app.py
+++ jobs/app.py
@@ -2,7 +2,6 @@ import sqlite3
 from unittest import result
 from flask import Flask, render_template, g 
 
-
 PATH='db/jobs.sqlite'
 app = Flask(__name__)
 
@@ -33,4 +32,5 @@ def close_connection(exception):
 @app.route('/')
 @app.route('/jobs')
 def jobs():
-  return render_template('index.html')
+  jobs = execute_sql('SELECT job.id, job.title, job.description, job.salary, employer.id as employer_id, employer.name as employer_name FROM job JOIN employer ON employer.id = job.employer_id')
+  return render_template('index.html',jobs=jobs)
