28a69f23579533456196b411fdd741c3c081f7ed
Joel Udeh <Joel.Udeh@impsoftware.co.uk>
2022-05-02 06:04:49 +0100

    Commit

--- jobs/app.py
+++ jobs/app.py
@@ -1,36 +1,40 @@
 import sqlite3
-from unittest import result
-from flask import Flask, render_template, g 
+from flask import Flask, render_template, g
 
-PATH='db/jobs.sqlite'
+PATH = 'db/jobs.sqlite'
+
 app = Flask(__name__)
 
 def open_connection():
-  connection = getattr(g, '_connection', None)
-  if connection == None:
-    connection = g._connection = sqlite3.connect(PATH)
-  connection.row_factory = sqlite3.Row
-  return connection
+    connection = getattr(g, '_connection', None)
+    if connection == None:
+        connection = g._connection = sqlite3.connect(PATH)
+    connection.row_factory = sqlite3.Row
+    return connection
 
 def execute_sql(sql, values=(), commit=False, single=False):
-  connection = open_connection()
-  cursor = connection.execute(sql,values)
-  if commit == True :
-    results = connection.commit()
-  else:
-      results =  cursor.fetchone() if single else cursor.fetchall()
-  cursor.close()
-  return results
+    connection = open_connection()
+    cursor = connection.execute(sql, values)
+    if commit == True:
+        results = connection.commit()
+    else:
+        results = cursor.fetchone() if single else cursor.fetchall()
+    cursor.close()
+    return results
 
 @app.teardown_appcontext
 def close_connection(exception):
-  connection = getattr(g, '_connection', None)
-  if connection is not None:
-      connection.close()
-
+    connection = getattr(g, '_connection', None)
+    if connection is not None:
+        connection.close()
 
 @app.route('/')
 @app.route('/jobs')
 def jobs():
-  jobs = execute_sql('SELECT job.id, job.title, job.description, job.salary, employer.id as employer_id, employer.name as employer_name FROM job JOIN employer ON employer.id = job.employer_id')
-  return render_template('index.html',jobs=jobs)
+    jobs = execute_sql('SELECT job.id, job.title, job.description, job.salary, employer.id as employer_id, employer.name as employer_name FROM job JOIN employer ON employer.id = job.employer_id')
+    return render_template('index.html', jobs=jobs)
+
+@app.route('/job/<job_id>')
+def job(job_id):
+    job = execute_sql('SELECT job.id, job.title, job.description, job.salary, employer.id as employer_id, employer.name as employer_name FROM job JOIN employer ON employer.id = job.employer_id WHERE job.id = ?', [job_id], single=True)
+    return render_template('job.html', job=job)
